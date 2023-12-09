089b48d68dfe1ced3b587feb5e481ede6b0bea1f
vanjaneya <vanjaneya@beckman.com>
2023-12-09 09:58:15 +0530

    Added product Repo and interfaces

---
+++ src/Services/Catalog/Catalog.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,55 @@
+﻿using Catalog.Api.Data;
+using Catalog.Api.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Catalog.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogContext _catalogContext;
+        public ProductRepository(ICatalogContext catalogContext)
+        {
+            _catalogContext = catalogContext ?? throw new ArgumentNullException(nameof(catalogContext));
+        }
+        public async Task CreateProduct(Product product)
+        {
+            await _catalogContext.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            var deleteOneResult = await _catalogContext.Products.DeleteOneAsync(filter: p => p.Id == id);
+            return deleteOneResult.IsAcknowledged && deleteOneResult.DeletedCount > 0;
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var replaceOneResult =  await _catalogContext.Products.ReplaceOneAsync(filter: p => p.Id == product.Id, replacement: product);
+            return replaceOneResult.IsAcknowledged && replaceOneResult.ModifiedCount > 0;
+        }
+
+        public async Task<Product> GetProduct(string id)
+        {
+            return await _catalogContext.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByCatagory(string categoryName)
+        {
+            return await _catalogContext.Products.Find(p => p.Category == categoryName).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
+        {
+            return await _catalogContext.Products.Find(p => p.Name == name).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _catalogContext.Products.Find(p => true).ToListAsync();
+        }       
+    }
+}
