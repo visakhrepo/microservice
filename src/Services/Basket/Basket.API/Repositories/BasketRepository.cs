4419ac180212c3dd7ec6d3a7ffd01a6c3fb6e373
vanjaneya <vanjaneya@beckman.com>
2023-12-20 14:18:56 +0530

    Repository added

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache cache;
+        public BasketRepository(IDistributedCache _cache)
+        {
+            cache = _cache;
+        }
+        public async Task DeleteBasket(string userName)
+        {
+            await cache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await cache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart cart)
+        {
+            await cache.SetStringAsync(cart.UserName, JsonConvert.SerializeObject(cart));
+            return await GetBasket(cart.UserName);
+        }
+    }
+}
